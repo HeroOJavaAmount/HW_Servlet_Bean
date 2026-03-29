10688b7b36712db5636b803677cca446b8a17217
Alexandr_O <aleksandr.osha@yahoo.com>
2026-03-29 12:46:55 +0300

    Update BeanConfig.java

--- src/main/java/ru/netology/config/BeanConfig.java
+++ src/main/java/ru/netology/config/BeanConfig.java
@@ -1,9 +1,25 @@
 package ru.netology.config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import ru.netology.controller.PostController;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
 
-@Configuration
-@ComponentScan("ru.netology")
+
 public class BeanConfig {
+
+    @Bean
+    public PostRepository postRepository(){
+    return new PostRepository();
+    }
+
+    @Bean
+    public PostService postService(){
+        return new PostService(postRepository());
+    }
+
+    @Bean
+    public PostController postController() {
+        return new PostController(postService());
+    }
 }
