1b45bc69caeef9e3a19a490d035832dd91606dd2
Aleksandr_A <aleksandr.osha@yahoo.com>
2026-03-29 11:45:05 +0300

    145551

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,6 +1,11 @@
 package ru.netology.servlet;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import ru.netology.config.BeanConfig;
 import ru.netology.controller.PostController;
+import ru.netology.exception.NotFoundException;
 import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
 
@@ -9,47 +14,62 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
-  private PostController controller;
 
-  @Override
-  public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
-  }
+    // Константы для маршрутов и методов (рефакторинг)
+    private static final String API_POSTS = "/api/posts";
+    private static final String API_POSTS_WITH_ID = "/api/posts/\\d+";
+    private static final String GET_METHOD = "GET";
+    private static final String POST_METHOD = "POST";
+    private static final String DELETE_METHOD = "DELETE";
 
-  @Override
-  protected void service(HttpServletRequest req, HttpServletResponse resp) {
-    // если деплоились в root context, то достаточно этого
-    try {
-      final var path = req.getRequestURI();
-      final var method = req.getMethod();
-      // primitive routing
-      if (method.equals("GET") && path.equals("/api/posts")) {
-        controller.all(resp);
-        return;
-      }
-      if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-        controller.getById(id, resp);
-        return;
-      }
-      if (method.equals("POST") && path.equals("/api/posts")) {
-        controller.save(req.getReader(), resp);
-        return;
-      }
-      if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-        controller.removeById(id, resp);
-        return;
-      }
-      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    } catch (Exception e) {
-      e.printStackTrace();
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    private PostController controller;
+
+    @Override
+    public void init() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
+        controller = context.getBean(PostController.class);
     }
-  }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            final var path = req.getRequestURI();   // например, "/api/posts" или "/api/posts/123"
+            final var method = req.getMethod();     // GET, POST, DELETE и т.д.
+
+            if (GET_METHOD.equals(method) && API_POSTS.equals(path)) {// Маршрутизация
+                controller.all(resp);
+                return;
+            }
+            if (GET_METHOD.equals(method) && path.matches(API_POSTS_WITH_ID)) {
+                long id = extractId(path);
+                controller.getById(id, resp);
+                return;
+            }
+            if (POST_METHOD.equals(method) && API_POSTS.equals(path)) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (DELETE_METHOD.equals(method) && path.matches(API_POSTS_WITH_ID)) {
+                long id = extractId(path);
+                controller.removeById(id, resp);
+                return;
+            }
+            // Если ни один маршрут не подошёл
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (NotFoundException e) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // Вспомогательный метод для извлечения id из пути (убираем дублирование)
+    private long extractId(String path) {
+        // path имеет вид "/api/posts/123"
+        String idPart = path.substring(path.lastIndexOf('/') + 1);
+        return Long.parseLong(idPart);
+    }
 }
+
 
